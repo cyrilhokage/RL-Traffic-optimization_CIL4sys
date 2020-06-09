e5eccbca2da9f3a1a233dee9f9f3e8f1b9937487
julienguegan <45081450+julienguegan@users.noreply.github.com>
2020-06-09 16:02:46 +0200

    clarify section by using class files

--- notebooks/Traffic_Lights/train_traffic_light.py
+++ notebooks/Traffic_Lights/train_traffic_light.py
@@ -15,187 +15,71 @@
 
 
 from flow.core.params import VehicleParams
+from flow.core.params import SumoCarFollowingParams, SumoLaneChangeParams
 from flow.core.params import NetParams
 from flow.core.params import InitialConfig
 from flow.core.params import EnvParams
 from flow.core.params import SumoParams
-from flow.controllers import RLController, IDMController
-from flow.networks import Network
+from flow.controllers import IDMController
 from flow.core.params import InFlows
 from collections import OrderedDict
-import json
 import ray
-from ray.rllib.agents.registry import get_agent_class
 from ray.tune import run_experiments
-from ray.tune.registry import register_env
-from flow.utils.registry import make_create_env
-from flow.utils.rllib import FlowParamsEncoder
-import numpy as np
 
-# ## Crée le network
-
+# ========= NETWORK ======= #
 # Specifie les noms des routes du network dont les vehicules peuvent être s'insérer
-
-# In[2]:
-
-
-EDGES_DISTRIBUTION = ["-100822066", "4794817", "4783299#0", "155558218"]
+# et les itinéraires possibles que les voitures utilisent
+  
+from Issy_Network import IssyOSMNetwork, EDGES_DISTRIBUTION
 
 
-# créer la classe Network pour spécifier les itinéraires possibles
-
-# In[3]:
-
-import random
-
-class IssyOSMNetwork(Network):
-
-    
-    def get_routes_list(self):
-        return list(self.specify_routes({}).keys())
-
-    def get_random_route(self):
-        return random.choice(self.get_routes_list())
-
-    def specify_routes(self, net_params):
-        return {
-            "-100822066": [ #N
-                "-100822066",
-                "-352962858#1",
-                "-352962858#0",
-                "-4786940#1",
-                 "-4786940#0",
-            ],
-            
-            "4794817" : [ #Loop
-                "4794817",
-                "4786972#0",
-                "4786972#1",
-                "4786972#2",
-                "4786965#1",
-                "4786965#2",
-                "4786965#3",
-                "4795729",
-                "-352962858#1",
-                "4795742#0",
-                "4795742#1",
-                "4786965#3",
-                "4786965#4",
-                "4786965#5",
-            ],
-            
-            "4783299#0": [    #E
-                "4783299#0",
-                "4783299#1",
-                "4783299#2",
-                "4783299#3",
-                "4783299#4",
-                "4783299#5",
-                "4783299#6",
-                "4786940#0",
-                "4786940#1",
-                "352962858#0",
-                "352962858#1",
-                "100822066",
-            ],
-            
-            "155558218": [
-                "155558218",
-                "4786940#1",
-                "352962858#0",
-                "352962858#1",
-                "100822066",
-            ],     
-        }
-
-
-# ## Ajoute les flux de voiture
-
-# `IDMController` : The Intelligent Driver Model is a car-following model specifying vehicle dynamics by a differential equation for acceleration $\dot{v}$.
-# 
-# `RLController` : a trainable autuonomous vehicle whose actions are dictated by an RL agent. 
-
-# In[4]:
-
-n_veh    = 12
+# ======= flux de voiture ====== #
+n_veh    = 10
 vehicles = VehicleParams()
-vehicles.add("human",acceleration_controller=(IDMController, {}), num_vehicles=n_veh, color='red')
-vehicles.add("flow",acceleration_controller=(IDMController, {}), num_vehicles=n_veh, color='white')
+vehicles.add("human", acceleration_controller=(IDMController, {}), num_vehicles=n_veh, car_following_params=SumoCarFollowingParams(speed_mode="right_of_way"),
+             lane_change_params=SumoLaneChangeParams(lane_change_mode=2722), color='red')
+vehicles.add("flow", acceleration_controller=(IDMController, {}), num_vehicles=n_veh, car_following_params=SumoCarFollowingParams(speed_mode="right_of_way"),
+             lane_change_params=SumoLaneChangeParams(lane_change_mode=2722), color='white')
 
-
-
-# - `vehs_per_hour`: nombre de vehicule par heure, uniformément espacés. Par exemple, comme il y a $60 \times 60 = 3600$ secondes dans une heure, le parametre $\frac{3600}{5}=720$ va faire rentrer des vehicules dans le network toutes les $5$ secondes.
-# 
-# - `probability`: c'est la probabilité qu'un véhicule entre dans le network toutes les secondes. Par exemple, si on la fixe à $0.2$, alors chaque seconde de la simulation un véhicule aura $\frac{1}{5}$ chance d'entrer dans le network
-# 
-# - `period`: C'est le temps en secondes entre 2 véhicules qui sont insérés. Par exemple, le fixer à $5$ ferait rentrer des véhicules dans le network toutes les $5$ secondes (ce qui équivaut à mettre `vehs_per_hour` à $720$).
-# 
-# <font color='red'>
-# $\rightarrow$ Exactement 1 seul de ces 3 paramètres doit être configurer !
-# </font>
-
-# In[5]:
 inflow = InFlows()
-for edge in EDGES_DISTRIBUTION:
-    inflow.add(veh_type = "flow", edge = edge, probability = 0.1, depart_speed = 7)
+inflow.add(veh_type="human", edge="4794817", probability=0.01, depart_speed=7, depart_lane=0)
+inflow.add(veh_type="human", edge="4783299#0", probability=0.2, depart_speed=7, depart_lane=0)
+inflow.add(veh_type="human", edge="-100822066", probability=0.25, depart_speed=7,depart_lane=0)
+inflow.add(veh_type="human",edge="155558218",probability=0.01, depart_speed=7, depart_lane=0)
 
 
-# ## Personnalise un Environnement pour le RL
-# 
-# plus de méthodes sur : http://berkeleyflow.readthedocs.io/en/latest/
-
-# In[6]:
-
-
+# ======= Environnement personnalisé pour RL ======= #
 from IssyEnv import IssyEnv1
 
 
-# ## Lance une simulation avec Training RLlib
-# 
-# Pour qu'un environnement puisse être entrainé, l'environnement doit être accessible via l'importation à partir de flow.envs. 
-# 
-# 
-# <font color='red'>
-# Copier l'environnement créé dans un fichier .py et on importe l'environnement dans `flow.envs.__init__.py`.
-# Mettre le chemin absolu du fichier .osm .
-# </font> 
+# ======= Lance un Training RLlib ======= #
 
-# In[7]:
+# actions possibles 
+action_spec = OrderedDict({ "30677963": [ "GGGGrrrGGGG", "rrrrGGGrrrr"], "30677810": [ "GGrr", "rrGG"]})
 
-
-# possibles actions
-action_spec = OrderedDict({ "30677963": [ "GGGGrrrGGGG", "rrrrGGGrrrr"],
-                            "30677810": [ "GGrr", "rrGG"]})
-
-
-# In[8]:
-
-
+# parameters
 horizon  = 1000
 SIM_STEP = 0.2
 rollouts = 10
 n_cpus   = 1
 
 
-# In[9]:
-
-
 # SUMO PARAM
-sumo_params = SumoParams(sim_step=SIM_STEP, render=False, restart_instance=True, print_warnings=False, no_step_log=False)
+sumo_params = SumoParams(sim_step=SIM_STEP, overtake_right=True, render=False, restart_instance=True, print_warnings=False, no_step_log=False)
 
 # ENVIRONMENT PARAM
 ADDITIONAL_ENV_PARAMS = {"beta": n_veh, "action_spec": action_spec, "algorithm": "DQN", "tl_constraint_min": 100,  "tl_constraint_max": 600, "sim_step": SIM_STEP}
 env_params = EnvParams(additional_params=ADDITIONAL_ENV_PARAMS, horizon=horizon, warmup_steps=1)
 
 # NETWORK PARAM
-path_file  = '/home/julien/projet_CIL4SYS/NOTEBOOKS/issy.osm'
-net_params = NetParams(inflows=inflow, osm_path=path_file) 
+path_osm  = '/home/julien/projet_CIL4SYS/NOTEBOOKS/issy.osm'
+net_params = NetParams(inflows=inflow, osm_path=path_osm) 
 
 # NETWORK
 network = IssyOSMNetwork
 
 # INITIAL CONFIG
-initial_config = InitialConfig(spacing="uniform",edges_distribution=EDGES_DISTRIBUTION)
+initial_config = InitialConfig(spacing="uniform", edges_distribution=EDGES_DISTRIBUTION)
 
 
 flow_params = dict( exp_tag   = "ISSY_traffic", 
@@ -209,124 +93,32 @@ flow_params = dict( exp_tag   = "ISSY_traffic",
                     initial   = initial_config)
 
 
-# # Setup RLlib library
+# # Configure RLlib library
 
-# Configures RLlib DQN algorithm to be used to train the RL model.
+from config_RLlib import setup_DQN_exp
 
-# In[10]:
-
-
-def setup_DQN_exp():
-
-    alg_run   = 'DQN'
-    agent_cls = get_agent_class(alg_run)
-    config    = agent_cls._default_config.copy()
-    config['num_workers']      = n_cpus
-    config['train_batch_size'] = horizon * rollouts
-    config['gamma']            = discount_rate
-    config['clip_actions']     = False  # FIXME(ev) temporary ray bug
-    config['horizon']          = horizon
-    config["hiddens"]          = [256]
-    config['model'].update({'fcnet_hiddens': [32, 32]})
-
-    # save the flow params for replay
-    flow_json = json.dumps(flow_params, cls=FlowParamsEncoder, sort_keys=True, indent=4)
-    config['env_config']['flow_params'] = flow_json
-    config['env_config']['run'] = alg_run
-
-    create_env, gym_name = make_create_env(params=flow_params, version=0)
-
-    # Register as rllib env
-    register_env(gym_name, create_env)
-    
-    return alg_run, gym_name, config
-
-
-# Configures RLlib PPO algorithm to be used to train the RL model.
-# 
-# See: https://ray.readthedocs.io/en/latest/rllib-algorithms.html#proximal-policy-optimization-ppo
-
-# In[11]:
-
-
-def setup_PPO_exp():
-
-    alg_run   = 'PPO'
-    agent_cls = get_agent_class(alg_run)
-    config    = agent_cls._default_config.copy()
-    config['num_workers']      = n_cpus
-    config['train_batch_size'] = horizon * rollouts
-    config['gamma']            = discount_rate # Discount factor of the MDP.
-    config['horizon']          = horizon
-    config['clip_actions']     = False  # FIXME(ev) temporary ray bug
-    config['model'].update({'fcnet_hiddens': [32, 32]})
-
-    # save the flow params for replay
-    flow_json = json.dumps(flow_params,cls=FlowParamsEncoder,sort_keys=True,indent=4)
-    config['env_config']['flow_params'] = flow_json
-    config['env_config']['run'] = alg_run
-
-    create_env, gym_name = make_create_env(params=flow_params,version=0)
-
-    # Register as rllib env
-    register_env(gym_name, create_env)
-    
-    return alg_run, gym_name, config
+alg_run, gym_name, config = setup_DQN_exp(flow_params, n_cpus, horizon, rollouts)
 
 
 # # Run Experiment
 
 # In[12]:
 
-
-alg_run, gym_name, config = setup_DQN_exp()
-
-ray.init(num_cpus=n_cpus, ignore_reinit_error=True, include_webui=True) # , local_mode=True)
+ray.init(num_cpus=n_cpus+1, ignore_reinit_error=True, include_webui=True, webui_host="") # , local_mode=True)
 
 
 
 # In[14]:
 
-# ======= DEFINE CALLBACKS ========= #
+# generate customize Callbacks 
+from callbacks import Customize_Callbacks
 
-def on_episode_start(info):
-    episode = info["episode"]
-    episode.user_data["accelerations"]  = []
-    episode.user_data["speeds"]         = []
-    episode.user_data["veh_wait_steps"] = []
-    episode.user_data["CO2_emissions"]  = []
-
-def on_episode_step(info):
-    episode = info["episode"]
-    acceleration  = np.mean(episode.last_observation_for()[:n_veh])
-    speed         = np.mean(episode.last_observation_for()[n_veh:2*n_veh])
-    CO2_emission  = np.mean(episode.last_observation_for()[2*n_veh:3*n_veh])
-    veh_wait_step = np.mean(episode.last_observation_for()[3*n_veh:4*n_veh])
-    episode.user_data["accelerations"].append(acceleration)
-    episode.user_data["speeds"].append(speed)
-    episode.user_data["CO2_emissions"].append(CO2_emission)
-    episode.user_data["veh_wait_steps"].append(veh_wait_step)
-
-def on_episode_end(info):
-    episode = info["episode"]
-    acceleration  = np.mean(episode.user_data["accelerations"])
-    speed         = np.mean(episode.user_data["speeds"])
-    CO2_emission  = np.mean(episode.user_data["CO2_emissions"])
-    veh_wait_step = np.mean(episode.user_data["veh_wait_steps"])
-    episode.custom_metrics["acceleration"]  = acceleration
-    episode.custom_metrics["speed"]         = speed
-    episode.custom_metrics["CO2_emission"]  = CO2_emission
-    episode.custom_metrics["veh_wait_step"] = veh_wait_step
+config["callbacks"] = Customize_Callbacks(n_veh)
 
 
+# In[14]:
 
-MyCallbacks = {"on_episode_start": on_episode_start,
-                "on_episode_step": on_episode_step,
-                "on_episode_end" : on_episode_end}
-
-config["callbacks"] = MyCallbacks
-
-
+# Run experiment
 exp_tag = {"run": alg_run,
            "env": gym_name,
            "config": {**config},
@@ -337,8 +129,7 @@ exp_tag = {"run": alg_run,
            #"resources_per_trial": {"cpu":2,"gpu":0}}
 
 
-
-trials = run_experiments({flow_params["exp_tag"]: exp_tag})#, queue_trials=True)
+trials = run_experiments({flow_params["exp_tag"]: exp_tag}, queue_trials=True)
 
 
 # In[ ]:
